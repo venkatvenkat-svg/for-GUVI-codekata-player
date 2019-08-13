c9642a6706d936ca914a7fb293c1e111640e29eb
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-08-13 21:02:12 +0530

    Create factorial_nums_in_20.py

---
+++ factorial_nums_in_20.py
@@ -0,0 +1,11 @@
+n=int(input())
+if(n<=20):
+    if n==0:
+        fact=1
+    else:
+        fact=1
+        for c in range(1,n+1):
+            fact=fact*c
+    print(fact)
+else:
+    print("no")
